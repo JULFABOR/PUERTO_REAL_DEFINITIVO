9ded3bc2c72138de7de59d6dd95ff545ee10c4ca
Leandro <abraham280618@gmail.com>
2025-07-14 19:16:25 -0300

    Add Burmese and Norwegian translations for Django messages
    - Created new translation files for Burmese (my) including django.po and django.mo.
    - Added translations for various messages and terms in the Django framework.
    - Created new translation files for Norwegian (nb) including django.mo.

--- Puerto_Real/settings.py
+++ Puerto_Real/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'productos',
+    'control_financiero',
+    'control_stock',
 ]
 
 MIDDLEWARE = [
